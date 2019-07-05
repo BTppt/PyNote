100c7af65a5d24fffef92274d9fbffef45d3ea57
BTppt <34875474+BTppt@users.noreply.github.com>
2019-07-05 21:04:24 +0800

    Initial commit

---
+++ Py/test/python1.py
@@ -0,0 +1,53 @@
+print('''
+Python 3.x                                              \n
+1 Data type                                             \n
+1.1 Integer                                             \n
+1.2 Floating Point                                      \n
+1.3 Complex number                                      \n
+1.4 String                                              \n
+1.5 Operators                                           \n
+1.6 List                                                \n
+1.7 Tuple                                               \n
+1.8 Dict                                                \n
+2 Control Structure                                     \n
+2.1 Selective Structure                                 \n
+2.2 Loop Structure                                      \n
+3 Function                                              \n
+3.1 Definition                                          \n
+3.2 Lambda Function                                     \n
+4 Class                                                 \n
+4.1 Definition                                          \n
+4.2 Inheritance and derive                              \n
+''')
+index = 1.1
+
+if index == 1.1 :
+    print('\n******************1.1 Integer\n')
+    binval = 0b10101
+    oval = 0o3721
+    xval = 0xffffa
+    dval = 999
+
+if index == 1.2 :
+    print('\n******************1.2 Floating Point\n')
+    dfloat = 9.999
+    sfloat = 3.14e5
+
+if index == 1.3 :
+    print('\n******************1.3 Complex number\n')    
+    cval = 3+2j
+    cval = complex(3,2)
+
+if index == 1.4 :
+    print('\n******************1.4 String\n')
+
+if index == 1.5 :
+    print('\n******************\n') 
+
+if index == 1.6 :
+    print('\n******************\n')
+
+print 'abc'
+
+if index == 1.1
+    print('\n******************\n')
---
+++ Py/test/python2.py
@@ -0,0 +1,9 @@
+# Special Library
+import tkinter
+
+for k in range(10)
+    print (k)
+
+a = 1
+if a==1
+    print(a)
