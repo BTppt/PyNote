13a633d59915dd66dc250d2cbb0a93ae4bb6174b
BTppt <34875474+BTppt@users.noreply.github.com>
2019-12-30 10:36:08 +0800

    fileio

--- 0-Data/FileIO.py
+++ 0-Data/FileIO.py
@@ -1,7 +1,6 @@
 from pathlib import *
 import os
-import json
-import pickle
+
 
 # file path
 file_path_s = PureWindowsPath('.\\resource\\test.txt')
@@ -28,19 +27,5 @@ with open(file_path, 'rb') as fp:
             break
         else:
             print(ch, end='')
-# JSON
-# obj1 = [{'year': 2018, "math": 90, "Chinese": 80, "is": {"physics": 100, "chem": 101}, 29: 57},
-#         {'year': 2019, "math": 95, "Chinese": 85, "is": {"physics": 105, "chem": 109}, 24: 67}]
-# print(obj1)
-# with open("./resource/test.json", 'w') as fp:
-#     json.dump(obj1, fp)
-# with open("./resource/test.json", 'r') as fp:
-#     obj2 = json.load(fp)
-# print(obj2)
 
-# pkl
-# obj3 = {}
-# for num in range(0, 10):
-#     obj3.update({num: num ** 2})
-# with open('./resource/test.pkl', 'w') as fp:
-#     pickle.dump(obj3, fp)
+
---
+++ 0-Data/PKLio.py
@@ -0,0 +1,8 @@
+import pickle
+
+# pkl
+# obj3 = {}
+# for num in range(0, 10):
+#     obj3.update({num: num ** 2})
+# with open('./resource/test.pkl', 'w') as fp:
+#     pickle.dump(obj3, fp)
